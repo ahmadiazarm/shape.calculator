13125b49936e40380bd4ed3ee07fa35b73badc83
ahmadiazarm <158315057+ahmadiazarm@users.noreply.github.com>
2024-02-16 15:56:55 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+
+public class area aaa {
+
+    public void start() {
+
+    }
+
+    private int calculateTriangleArea(String type, int d1, int d2, int d3) {
+        return 0;
+    }
+
+    private int square(int d1, int d2) {
+        return 0;
+    }
+
+    private int rectangle(int d1, int d2) {
+        return 0;
+    }
+
+    private int circle(int r) {
+        return 0;
+    }
+}
---
+++ ShapeCalculator.java
@@ -0,0 +1,17 @@
+public class ShapeCalculator {    public static void main(String[] args) {
+    // 1 calculate the following areas:
+    // * triangle
+    // * square
+    // * circle
+    // * rectangle
+    // first get the shape name from the user
+    // second get the dimensions of the shapes
+    // third returns the calculated area
+    // forth asks the user if they want to calculate for another shape or exit the program
+
+
+    ShapeCalculator shapeCalculator = new ShapeCalculator();
+    shapeCalculator.start();
+}
+}
+
